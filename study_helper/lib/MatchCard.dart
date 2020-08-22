18184d252a52ae49a73cae13c79ff96b11f35cd7
Clément Sicard <clementsicard@MacBook-Pro-de-Clement.local>
2020-08-22 13:39:36 +0200

    updated structure

---
+++ study_helper/lib/MatchCard.dart
@@ -0,0 +1,13 @@
+class MatchCard {
+  int redColor = 0;
+  int greenColor = 0;
+  int blueColor = 0;
+  double margin = 0;
+
+  MatchCard(int red, int green, int blue, double marginTop) {
+    redColor = red;
+    greenColor = green;
+    blueColor = blue;
+    margin = marginTop;
+  }
+}
