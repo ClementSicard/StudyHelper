fbe58766a722d1da758019ea2fd2ca635e649431
Clément Sicard <clement.sicard@epfl.ch>
2020-09-01 16:48:23 +0200

    modified structure

--- study_helper/lib/home_page.dart
+++ study_helper/lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
--- study_helper/lib/main.dart
+++ study_helper/lib/main.dart
@@ -1,103 +1,36 @@
 import 'package:flutter/material.dart';
-import 'MatchCard.dart';
+import 'package:provider/provider.dart';
+import 'package:study_helper/utils/change_notifier_list.dart';
 
-void main() => runApp(MyApp());
+import 'home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
         primarySwatch: Colors.blue,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Card Stack'),
+      home: MyHomePage(title: 'StudyHelper'),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  List<Widget> cardList;
-
-  void _removeCard(index) {
-    setState(() {
-      cardList.removeAt(index);
-    });
-  }
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    cardList = _getMatchCard();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Stack(
-          alignment: Alignment.center,
-          children: cardList,
-        ),
-      ),
-    );
-  }
-
-  List<Widget> _getMatchCard() {
-    List<MatchCard> cards = new List();
-    cards.add(MatchCard(255, 0, 0, 10));
-    cards.add(MatchCard(0, 255, 0, 20));
-    cards.add(MatchCard(0, 0, 255, 100));
-
-    List<Widget> cardList = new List();
-
-    for (int x = 0; x < 3; x++) {
-      cardList.add(Positioned(
-        top: cards[x].margin,
-        child: Draggable(
-          onDragEnd: (drag) {
-            _removeCard(x);
-          },
-          childWhenDragging: Container(),
-          feedback: Card(
-            elevation: 12,
-            color: Color.fromARGB(255, cards[x].redColor, cards[x].greenColor,
-                cards[x].blueColor),
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-            child: Container(
-              width: 240,
-              height: 300,
-            ),
-          ),
-          child: Card(
-            elevation: 12,
-            color: Color.fromARGB(255, cards[x].redColor, cards[x].greenColor,
-                cards[x].blueColor),
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-            child: Container(
-              width: 240,
-              height: 300,
-            ),
-          ),
-        ),
-      ));
-    }
-
-    return cardList;
   }
 }
