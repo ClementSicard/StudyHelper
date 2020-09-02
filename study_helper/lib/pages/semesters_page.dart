138226b9988223be58a7eb70f8a7f307237cbb06
Clément Sicard <clement.sicard@epfl.ch>
2020-09-02 16:56:43 +0200

    new page

---
+++ study_helper/lib/pages/semesters_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SemestersPage extends StatefulWidget {
+  SemestersPage({Key key}) : super(key: key);
+
+  @override
+  _SemestersPageState createState() => _SemestersPageState();
+}
+
+class _SemestersPageState extends State<SemestersPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
