1f26e03b2ff26f4d1dcaab9dcea7e46659602db1
Clément Sicard <clement.sicard@epfl.ch>
2020-09-05 09:47:52 +0200

    improvements and csv to array implementation start

--- study_helper/lib/csv_to_2d_array.dart
+++ study_helper/lib/csv_to_2d_array.dart
@@ -1,10 +1,37 @@
 import 'dart:io';
 import 'package:study_helper/semester.dart';
 
-List<List<Subject>> CSVto2DArray(File csv) {
+List<Chapter> CSVto2DArray(File csv, {String delimitator = ";"}) {
   if (csv == null)
     return null;
   else {
     List<String> contents = csv.readAsLinesSync();
+    int nbOfChapters = contents[0].split(";").length;
+    if (!checkCorrectCSV(contents, nbOfChapters)) {
+      return null;
+    } else {
+      List<Chapter> chapters = List();
+      List<String> firstLine = contents[0].split(delimitator);
+      for (int i = 0; i < nbOfChapters; i++) {
+        List<Subject> subList = List();
+        for (int j = 0; j < contents.length; j++) {
+          List<String> currentLine = contents[j].split(delimitator);
+          // Initially all the subjects are poorly mastered
+          subList.add(Subject(currentLine[i], Mastered.Poorly));
+        }
+        chapters.add(Chapter(firstLine[i], subList));
+      }
+      return chapters;
+    }
   }
+}
+
+bool checkCorrectCSV(List<String> csv, int nbOfChapters,
+    {String delimitator = ";"}) {
+  for (String s in csv) {
+    if (s.split(delimitator).length != nbOfChapters) {
+      return false;
+    }
+  }
+  return true;
 }
--- study_helper/lib/semester.dart
+++ study_helper/lib/semester.dart
@@ -30,10 +30,18 @@ class Semester {
 
 class Course {
   String _name;
-  List<List<Subject>> _subjects;
+  List<Chapter> _chapters;
 
   List<List<Subject>> get subjects {
-    return List.from(_subjects);
+    List<List<Subject>> list = List();
+    for (Chapter chapter in _chapters) {
+      list.addAll(chapter.subjects);
+    }
+    return list;
+  }
+
+  List<Chapter> get chapters {
+    return List.from(_chapters);
   }
 
   String get name {
@@ -45,4 +53,54 @@ class Course {
   }
 }
 
-class Subject {}
+class Chapter {
+  String _name;
+  List<Subject> _subjects;
+
+  Chapter(String name, List<Subject> subjects) {
+    this._name = name;
+    this._subjects = List.from(subjects);
+  }
+
+  List<List<Subject>> get subjects {
+    return List.from(_subjects);
+  }
+
+  bool addSubject(Subject subject) {
+    if (_subjects.contains(subject)) return false;
+    _subjects.add(subject);
+    return true;
+  }
+
+  String get name {
+    return _name;
+  }
+
+  set name(String newName) {
+    _name = newName;
+  }
+}
+
+class Subject {
+  String _name;
+  Mastered _mas;
+
+  Subject(String name, Mastered mas) {
+    this._name = name;
+    this._mas = mas;
+  }
+
+  set mas(Mastered mas) {
+    _mas = mas;
+  }
+
+  String get name {
+    return _name;
+  }
+
+  set name(String name) {
+    _name = name;
+  }
+}
+
+enum Mastered { Good, Moderately, Poorly }
