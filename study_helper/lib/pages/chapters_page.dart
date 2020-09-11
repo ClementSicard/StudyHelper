d282ce710c58b9909a6479be492e902b047e338e
Clément Sicard <clement.sicard@epfl.ch>
2020-09-11 10:26:25 +0200

    fixed link to chapters page

--- study_helper/lib/pages/chapters_page.dart
+++ study_helper/lib/pages/chapters_page.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:study_helper/objects/course.dart';
+import 'package:study_helper/utils/custom_text_styles.dart';
 
 class ChaptersPage extends StatefulWidget {
   final Course _course;
@@ -14,14 +16,52 @@ class ChaptersPage extends StatefulWidget {
   }) : super(key: key);
 
   @override
-  _ChaptersPageState createState() => _ChaptersPageState();
+  _ChaptersPageState createState() => _ChaptersPageState(_course);
 }
 
 class _ChaptersPageState extends State<ChaptersPage> {
+  final Course _course;
+
+  _ChaptersPageState(this._course);
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Scaffold(),
+    return Scaffold(
+      appBar: AppBar(
+        brightness: Brightness.light,
+        elevation: 0,
+        centerTitle: true,
+        title: Text(
+          _course.name,
+          textAlign: TextAlign.center,
+          style: customTextStyle(),
+        ),
+        leading: IconButton(
+          icon: Icon(
+            CupertinoIcons.back,
+            color: Colors.black,
+            size: 30,
+          ),
+          tooltip: "Back",
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        backgroundColor: Colors.white,
+        actions: [
+          IconButton(
+            icon: Icon(
+              CupertinoIcons.add_circled,
+              color: Colors.black,
+              size: 30,
+            ),
+            color: Colors.white,
+            tooltip: "Add new course",
+            onPressed: () {},
+          ),
+        ],
+      ),
+      backgroundColor: Colors.white,
     );
   }
 }
