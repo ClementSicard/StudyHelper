56a14c69ffaa711c84f651fa5130786048b7413b
Clément Sicard <clement.sicard@epfl.ch>
2020-09-12 16:40:33 +0200

    added course prompt page

---
+++ study_helper/lib/pages/course_prompt_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CoursePromptPage extends StatefulWidget {
+  CoursePromptPage({Key key}) : super(key: key);
+
+  @override
+  _CoursePromptPageState createState() => _CoursePromptPageState();
+}
+
+class _CoursePromptPageState extends State<CoursePromptPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
