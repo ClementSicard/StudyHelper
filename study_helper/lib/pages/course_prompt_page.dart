31717f58d88e7eef9cf2f612b257ba53ebf8d5f3
Clément Sicard <clement.sicard@epfl.ch>
2020-09-13 14:25:30 +0200

    changes + pubspec

--- study_helper/lib/pages/course_prompt_page.dart
+++ study_helper/lib/pages/course_prompt_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:study_helper/utils/custom_text_styles.dart';
 
 class CoursePromptPage extends StatefulWidget {
   CoursePromptPage({Key key}) : super(key: key);
@@ -10,6 +12,48 @@ class CoursePromptPage extends StatefulWidget {
 class _CoursePromptPageState extends State<CoursePromptPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        brightness: Brightness.light,
+        elevation: 0,
+        centerTitle: true,
+        title: Text(
+          "Add a new course",
+          textAlign: TextAlign.center,
+          style: customTextStyle(),
+        ),
+        leading: IconButton(
+          icon: Icon(
+            CupertinoIcons.back,
+            color: Colors.black,
+            size: 30,
+          ),
+          tooltip: "Back",
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        backgroundColor: Colors.white,
+        actions: [
+          IconButton(
+            icon: Icon(
+              CupertinoIcons.add_circled,
+              color: Colors.black,
+              size: 30,
+            ),
+            color: Colors.white,
+            tooltip: "Add new course",
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => CoursePromptPage(),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+      backgroundColor: Colors.white,
+    );
   }
 }
