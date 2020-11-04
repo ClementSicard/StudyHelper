6cf45a6dc43e83d7c7dc91f042e4dd46c996081d
Clément Sicard <clement.sicard@epfl.ch>
2020-11-04 14:35:11 +0100

    new way to select dark mode

--- study_helper/lib/pages/settings_page.dart
+++ study_helper/lib/pages/settings_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 import 'package:provider/provider.dart';
 import 'package:study_helper/objects/dark_theme_handler.dart';
 import 'package:study_helper/utils/custom_text_styles.dart';
@@ -35,10 +36,27 @@ class _SettingsPageState extends State<SettingsPage> {
         body: Padding(
           padding: const EdgeInsets.all(20.0),
           child: Center(
-            child: Text(
-              "No settings available at the moment.",
-              style: customTextStyle(themeChange.darkTheme),
-              textAlign: TextAlign.center,
+            child: ListView(
+              children: <Widget>[
+                ListTile(
+                  leading: Icon(!themeChange.darkTheme
+                      ? Icons.wb_sunny
+                      : Ionicons.ios_moon),
+                  title: Text(
+                    "Dark mode",
+                    style: customTextStyle(themeChange.darkTheme),
+                    textAlign: TextAlign.center,
+                  ),
+                  trailing: CupertinoSwitch(
+                    onChanged: (bool value) {
+                      setState(() {
+                        themeChange.darkTheme = value;
+                      });
+                    },
+                    value: themeChange.darkTheme,
+                  ),
+                ),
+              ],
             ),
           ),
         ),
